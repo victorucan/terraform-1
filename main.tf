69d84137760b3362d0294814e493d350e44059d3
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-09 23:25:19 +0000

    update

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "pri_vpc" {
+  cidr_block           = "172.2.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.pri_vpc.id
+  cidr_block = "172.2.0.0/24"
+
+  tags = {
+    Name = "sub1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.pri_vpc.id
+  cidr_block = "172.2.0.0/24"
+
+  tags = {
+    Name = "sub2"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-2"
+  access_key = ${ACCESSKEY}
+  secret_key = ${SECRETKEY}
+}
