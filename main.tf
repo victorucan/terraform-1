d2381b6842f8146c7daec4655ee04c8756afc216
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-10 23:07:22 +0000

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "pri_vpc" {
-  cidr_block           = "172.2.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -10,7 +10,7 @@ resource "aws_vpc" "pri_vpc" {
 
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.pri_vpc.id
-  cidr_block = "172.2.0.0/24"
+  cidr_block = "10.1.1.0/24"
 
   tags = {
     Name = "sub1"
@@ -19,7 +19,7 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
   vpc_id     = aws_vpc.pri_vpc.id
-  cidr_block = "172.2.1.0/24"
+  cidr_block = "10.1.2.0/24"
 
   tags = {
     Name = "sub2"
@@ -36,7 +36,7 @@ resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.pri_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
@@ -90,8 +90,8 @@ resource "aws_instance" "web" {
   tags = {
     Name = "nginx"
   }
- 
+  
   root_block_device {
-    volume_size = 10
+    volume_size= 10
   }
 }
