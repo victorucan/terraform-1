943b84b4310492c942046eefede723be31de36f1
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-10 15:44:28 +0000

    route associations

--- main.tf
+++ main.tf
@@ -43,3 +43,13 @@ resource "aws_route_table" "rt" {
     Name = "route_table"
   }
 }
+
+resource "aws_route_table_association" "as1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "as2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
