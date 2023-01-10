efb39f7862f5113e90e742be3a41b23cc504e223
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-10 19:03:57 +0000

    security groups

--- main.tf
+++ main.tf
@@ -54,22 +54,42 @@ resource "aws_route_table_association" "as2" {
   route_table_id = aws_route_table.rt.id
 }
 
+resource "aws_security_group" "dev_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.pri_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_key_pair" "key" {
   key_name   = "mykey"
   public_key = file("/home/ubuntu/.ssh/mykey.pub")
 }
 
 resource "aws_instance" "web" {
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   ami           = "ami-0ff39345bd62c82a5"
+  key_name = aws_key_pair.key.id
+  subnet_id = aws_subnet.subnet1.id
+  user_data = file("userdata.tpl")
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
 
   tags = {
     Name = "nginx"
   }
-  key_name  = aws_key_pair.key.id
-  subnet_id = aws_subnet.subnet1.id
-  user_data = file("userdata.tpl")
-
+ 
   root_block_device {
     volume_size = 10
   }
