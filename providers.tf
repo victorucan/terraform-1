f65add92af68ffb74950c6e55c657c0d00b3c8f5
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-10 00:29:07 +0000

    update

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 provider "aws" {
-  region = "us-east-2"
+  shared_config_files      = ["/home/ubuntu/.aws/config"]
   shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
   profile = "default"
 }
