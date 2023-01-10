aeb3ca185c9979e60ae8e2b41a0cfb188a0c3963
Ubuntu <ubuntu@ip-172-31-4-0.us-east-2.compute.internal>
2023-01-10 14:50:51 +0000

    rt y gw

--- main.tf
+++ main.tf
@@ -25,3 +25,21 @@ resource "aws_subnet" "subnet2" {
     Name = "sub2"
   }
 }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.pri_vpc.id
+
+  tags = {
+    Name = "gw"
+  }
+}
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.pri_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "route_table"
+  }
+}
